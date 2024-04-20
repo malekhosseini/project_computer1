13392665e8ccefaa986f0ce5aed148dfed7dd6ea
seyed mohammad bagher malekhosseini <malekhosseini@users.noreply.github.com>
2024-04-20 16:58:53 +0330

    ذخیره داده دانشجوی جدید در فایل
    در این نسخه کدهایی برای ذخیره دیتای دانشجوی جدید در فایل اضافه شده است. این بخش در تابعی اضافه شد که قبلا از کاربر داده دانشجوی جدید را دریافت میکرد و در یک آرایه در برنامه ذخیره مینمود. اکنون این داده ها در یک فایل نیز ذخیره میشوند

--- struct0.cpp
+++ struct0.cpp
@@ -1,28 +1,41 @@
 #include<iostream>
+#include <fstream>
 using namespace std;
+/// یک شی از نوع فایل خروجی برای ذخیره سازی داده های دانشجویان
+ofstream MyFile("students_data.txt");
+/// @brief این ساختار دیتای یک دانشجو در کد برنامه است
 struct student
 {
     string name,family;
-    int id;
+    int id;// شماره دانشجویی
     float math,phy,fa,mid;
     int ststus; //0 ststus<12 mashrot //1 12<=ststus<19 ghabool //2 ststus<=19 derakhshan   
 };
+/// @brief این تابع برای گرفتن داده های یک دانشجوی جدید از کاربر طراحی شده است
+/// @return اطلاعات یک دانشجوی جدید
 student get_student_data()
 {
     student New;
     cout<<"inter ID number:";
     cin>>New.id;
+    MyFile<<New.id<<endl;
     cout<<"inter name:";
     cin>>New.name;
+    MyFile<<New.name<<endl;
     cout<<"inter family:";
     cin>>New.family;
+    MyFile<<New.family<<endl;
 
     cout<<"inter physics number:";
     cin>>New.phy;
+    MyFile<<New.phy<<endl;
     cout<<"inter math number:";
     cin>>New.math;
+    MyFile<<New.math<<endl;
     cout<<"inter farsi number:";
     cin>>New.fa;
+    MyFile<<New.fa<<endl;
+    MyFile<<endl;
 
     return New;
 }
@@ -36,6 +49,9 @@ void print_studen_data(student data)
     cout<<"student farsi numner is: "<< data.fa<<endl;
     cout<<"student middel is: "<< data.mid<<endl;
 }
+/// @brief این تابع مقدار معدل دانشجو را محاسبه میکند
+/// @param data 
+/// @return مقدار خروجی معدل دانشجویی است که به عنوان ورودی به تابع داده شده است
 float hesab_mid(student data)
 {
     data.mid=(data.fa+data.math+data.phy)/3;
@@ -48,6 +64,8 @@ int main()
 {
     student new_students[10];
     new_students[0]=get_student_data();
+    new_students[1]=get_student_data();
+    new_students[2]=get_student_data();
     print_studen_data(new_students[0]);
     float a=hesab_mid(new_students[0]);
     cout<<new_students[0].mid<<endl;
